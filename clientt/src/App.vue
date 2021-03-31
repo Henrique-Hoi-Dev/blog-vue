bc5829275ce908d77210feb9faff172c63df0148
Henrique Hoinacki <riqueah@gmail.com>
2021-03-31 15:19:20 -0300

    changes

---
+++ clientt/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Menu></Menu>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Menu from '@/components/Menu.vue';
+
+export default {
+  components: { Menu }
+}
+</script>
+
+
+<style >
+body{
+  background-color: #f0efe7 !important;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  margin: 20px;
+}
+#nav a {
+  font-weight: bold;
+  color: #0056b3;
+  text-decoration: none;
+}
+#nav a:hover {
+  transition: 0.5s;
+  opacity: 0.5;
+}
+.b-sidebar:not(.b-sidebar-right) {
+  max-width: 200px;
+}
+</style>
---
+++ clientt/src/views/Text.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+        <UserText />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserText from "@/components/UserText.vue";
+
+export default {
+  components: { UserText },
+};
+</script>
